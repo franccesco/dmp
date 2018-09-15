23bf9aabf97fa76e5d62d2b71698b41ce235462b
Franccesco Orozco <franccesco@codingdose.info>
2018-09-15 17:29:42 -0600

    add coverage testin

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "dmp"
+require 'coveralls'
 require "minitest/autorun"
 require 'minitest/reporters'
+Coveralls.wear!
 Minitest::Reporters.use!
