1f4e50cd7c2a5684de0f8e532f65d17c7b6d5491
Franccesco Orozco <franccesco@codingdose.info>
2018-09-19 12:15:43 -0600

    fix copy to clipboard function

--- lib/dmp.rb
+++ lib/dmp.rb
@@ -1,4 +1,6 @@
 require 'dmp/version'
+require 'net/http'
+require 'digest/sha1'
 
 # Module to manage DMP operations
 module Dmp
@@ -13,5 +15,21 @@ module Dmp
     # randomize wordlist and strip it to the desired length
     random_pass = pass_list.shuffle[0...pass_length]
     random_pass
+  end
+
+  def check_pwned(passphrase)
+    passphrase_string = passphrase.join(' ')
+    hex_pass = Digest::SHA1.hexdigest(passphrase_string)
+    hex_pass_sample = hex_pass[0...5]
+    hex_pass_rest = hex_pass[5..-1]
+
+    # request a sample to HIBP to avoid disclosing the full pwd
+    uri = URI("https://api.pwnedpasswords.com/range/#{hex_pass_sample}")
+    req = Net::HTTP.get(uri)
+
+    clean_list = req.split("\r\n")
+    pass_list = clean_list.map { |hash| hash.split(':') }
+    pass_hash = Hash[*pass_list.flatten!]
+    pass_hash[hex_pass_rest.upcase]
   end
 end
--- test/dmp_test.rb
+++ test/dmp_test.rb
@@ -2,16 +2,33 @@ require "test_helper"
 require 'dmp'
 
 class DmpTest < Minitest::Test
+
+  def setup
+    @unsafe_pass = ['passw0rd']
+    @safe_pass = Dmp.gen_passphrase(pass_length = 12)
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Dmp::VERSION
   end
 
   def test_if_gen_passphrase_respects_length
+    # gen_passphrase should respect prassphrase length
     passphrase3 = Dmp.gen_passphrase(3)
     passphrase_default = Dmp.gen_passphrase
     passphrase12 = Dmp.gen_passphrase(12)
     assert_equal passphrase3.length, 3, 'Passphrase length != 3'
     assert_equal passphrase_default.length, 7, 'Passphrase length != 7'
     assert_equal passphrase12.length, 12, 'Passphrase length != 12'
+  end
+
+  def test_vulnerable_pass
+    # check_pwned should flag this password
+    vuln_count = Dmp.check_pwned(@unsafe_pass)
+    refute_nil vuln_count
+  end
+
+  def test_safe_pass
+    
   end
 end
